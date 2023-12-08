98ff7a5c72cb3b799c5b7d6cc7bc98a30d7a047a
Amir <60965542+amir4rab@users.noreply.github.com>
2023-12-08 11:26:35 -0500

    added vitest and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/__test__/setup.ts",
+    css: true,
+  },
+});
